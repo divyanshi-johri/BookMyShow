044b6c880fa0330c733260c8b96aa7e5ce60579a
Divuu <86949530+divyanshiJohari@users.noreply.github.com>
2021-10-20 18:33:18 +0530

    Update EntertainmentCard.component.js

--- src/components/Entertainment/EntertainmentCard.component.js
+++ src/components/Entertainment/EntertainmentCard.component.js
@@ -30,28 +30,81 @@ const EntertainmentCardSlider = () => {
 
     ];
 
-    const settings = {
-        autoplay: false,
-        infinite: true,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        InitialSlide: 0,
-        nextArrow: (
-            <div>
-              <div className='next-slick-arrow bg-black   bg-opacity-50 backdrop-filter rounded-full  absolute'>
-                <BiChevronRight/>
-              </div>
-            </div>
-          ),
-          prevArrow: (
-            <div>
-              <div className='prev-slick-arrow bg-black  bg-opacity-50 backdrop-filter rounded-full  absolute'>
-                <BiChevronLeft/>
-              </div>
-            </div>
-          )
+   const settings = {
 
-    }
+      autoplay: true,
+      speed:300,
+      infinite: true,
+      slidesToShow: 5,
+      slidesToScroll: 2,
+      InitialSlide: 0,
+  
+      nextArrow: (
+          <div>
+            <div className='next-slick-arrow bg-black  p-1  bg-opacity-50 backdrop-filter rounded-full absolute'>
+              <BiChevronRight/>
+            </div>
+          </div>
+        ),
+        prevArrow: (
+          <div>
+            <div className='prev-slick-arrow bg-black p-1  bg-opacity-50 backdrop-filter rounded-full  absolute'>
+              <BiChevronLeft/>
+            </div>
+          </div>
+        ),
+      responsive: [{
+              breakpoint: 1024,
+              settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 3,
+                  nextArrow: (
+                      <div>
+                        <div className='next-slick-arrow bg-black   bg-opacity-50 backdrop-filter rounded-full  absolute'>
+                          <BiChevronRight/>
+                        </div>
+                      </div>
+                    ),
+                    prevArrow: (
+                      <div>
+                        <div className='prev-slick-arrow bg-black  p-1 bg-opacity-50 backdrop-filter rounded-full  absolute'>
+                          <BiChevronLeft/>
+                        </div>
+                      </div>
+                    )
+              }
+          },
+  
+          {
+              breakpoint: 600,
+              settings: {   
+                  slidesToShow: 2,
+                  slidesToScroll: 2
+              }
+          },
+          {
+              breakpoint: 400,  
+              settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                  nextArrow: (
+                      <div>
+                        <div className='next-slick-arrow bg-black   bg-opacity-50 backdrop-filter rounded-lg  absolute'>
+                          <BiChevronRight/>
+                        </div>
+                      </div>
+                    ),
+                    prevArrow: (
+                      <div>
+                        <div className='prev-slick-arrow bg-black  bg-opacity-50 backdrop-filter rounded-lg  absolute'>
+                          <BiChevronLeft/>
+                        </div>
+                      </div>
+                    )
+              }
+          }
+      ]
+  };
 
 
     return ( 
